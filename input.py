a3d94dd630b5e1728d4841eafd423f08dcfe2d8e
Taib-Diallo <taiboudiallo225@gmail.com>
2022-08-12 15:10:36 -0400

    Updated code

---
+++ input.py
@@ -0,0 +1,18 @@
+# python list 
+myList= ['hi' , 'hello' , 23, 'well']
+#you need to have brackets for a list
+
+myList[1]
+print(myList[1])
+
+#to add something to list 
+myList.append("wow")
+
+print(myList[-1])
+print(myList[4])
+
+#tupple is unchangeable or immutable 
+myTuple= ("hello", "its me", 45)
+
+
+
